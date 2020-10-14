bcead3d9c3334279b5fa7b5f30beb78e585f378d
vicky1234-dev <66732822+vicky1234-dev@users.noreply.github.com>
2020-10-14 16:15:54 +0530

    Updates

---
+++ precache-manifest.47448336be03accbeb9a3bb92b9de583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8651cee67e6dbbc8697",
+    "url": "/reactsecondapp/static/css/main.8836a31c.chunk.css"
+  },
+  {
+    "revision": "f8651cee67e6dbbc8697",
+    "url": "/reactsecondapp/static/js/main.f8651cee.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/reactsecondapp/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "17952a73c175ae141908",
+    "url": "/reactsecondapp/static/js/runtime~main.17952a73.js"
+  },
+  {
+    "revision": "7f69bd4af4a405fc2ff5edf8ac0640f0",
+    "url": "/reactsecondapp/index.html"
+  }
+];
